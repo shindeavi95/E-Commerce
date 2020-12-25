b52f83b062809dc4ddd696f9f9f225ade1bf3dde
Avinash <avinashshinde999.as@gmail.com>
2020-12-25 20:37:00 +0530

    This is secound day on project

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,5 @@
-
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.urls import path
 
 from store import views
@@ -8,3 +9,5 @@ urlpatterns = [
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
 ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
+from store.models import *
 
 
 def store(request):
-    return render(request, "store/store.html")
+    products = Product.objects.all()
+    return render(request, "store/store.html", {"products": products})
 
 
 def cart(request):
-    return render(request, "store/cart.html")
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()  # find out which method it is ?
+    else:
+        items = []
+        order = {"get_cart_items": 0, "get_cart_total": 0}
+    context = {"items": items, "order": order}
+    return render(request, "store/cart.html", context)
 
 
 def checkout(request):
-    return render(request, "store/checkout.html")
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()  # find out which method it is ?
+    else:
+        items = []
+        order = {"get_cart_items": 0, "get_cart_total": 0}
+    context = {"items": items, "order": order}
+    return render(request, "store/checkout.html", context)
